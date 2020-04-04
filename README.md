7d5ea701bfd2caddd5b6469688b1d9665c0e0c6c
CynthiaEspejo <cynthiaespejo14@hotmail.com>
2020-04-04 06:34:23 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Cynthia1985.github.io
+# Hangman Programming Language
+
+#### demo:  https://cynthia1985.github.io/
+
+## Technologies Used
+# HTML
+# CSS
+# Vanilla JavaScript
+
+## Concept
+# When the user first gets to the page they have to guess the programming language word. The word is randomly chosen at the start of the game.
+
+## Game Play
+# To guess a letter the user clicks on any of the letters displayed. If the letter guessed is in the word, the letter shows up in the screen. If it is not there, a part of the hangman body will display and grows.
+
+## Loser
+# If the user runs out of guesses (6), the user will lose "You Lost". If they hit reset the page is refreshed and they will start a new game.
+
+## Winner
+# If the user guesses the word correctly before hangman is hung, a congratulatory message is displayed "You Won".
+
+## Challenges
+# Creating the alphabetic buttons from javaScript.
+# Growing the Hangman using 6 differents images.
---
+++ app.js
@@ -0,0 +1,111 @@
+
+// array of words
+var programming_languages = [
+	"python",
+	"javascript",
+	"mongodb",
+	"json",
+	"java",
+	"html",
+	"css",
+	"c",
+	"csharp",
+	"golang",
+	"kotlin",
+	"php",
+	"sql",
+	"ruby"
+]
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+// randomly the word to guess will be chosen from the array above
+function randomWord() {
+    answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
+
+// create the alphabetic buttons
+function generateButtons() {
+    let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
+      `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+  
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+// check if the the letter provided by the user matches one or more of the letters in the word
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+  
+    if (answer.indexOf(chosenLetter) >= 0) {
+      guessedWord();
+      checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+      mistakes++;
+      updateMistakes();
+      checkIfGameLost();
+      updateHangmanPicture();
+    }
+}
+
+// if you're losing the hangman grows
+function updateHangmanPicture() {
+    document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpeg';
+}
+
+// check if all letters have been found
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+      document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    }
+}
+
+// once you got six wrong letters, you lose
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+      document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+      document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    }
+}
+
+// prints the guessfield and check the word status
+function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+  
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+// if a guessed letter is not in the word, the letter will be put on the "wrong guesses"
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+// starts a new game hitting reset button
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/0.jpeg';
+  
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
